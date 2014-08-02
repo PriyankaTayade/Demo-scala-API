08f205add67ebc0ae89b75e2182fb565cb2437c0
PriyankaTayade <ptayade@scu.edu>
2014-08-02 12:17:29 -0700

    get client location 2 aug 14

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,7 +12,20 @@ object Application extends Controller {
     implicit request =>
     val address =  request.remoteAddress
     Logger.info("address " + address)
-  Ok("Got request [" + address + "]")
+    val json = ipInfo(address, "")
+  Ok("Got request [" + address +json+ "]")
   }
+  
+  def ipInfo(ip: String, key: String) = {
+      
+    val url = new java.net.URL("http://api.ipinfodb.com/v3/ip-city/?key=" + 
+            key + "&ip=" + ip + "&format=json")
+    val connection = url.openConnection
+    val inputStream = connection.getInputStream
+    val location = new String(Stream.continually(inputStream.read)
+            .takeWhile(-1 !=).map(_.toByte).toArray)
+    inputStream.close()
+    location
+}
 
 }
