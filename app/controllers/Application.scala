5e78e2edaa9697120fea31a79fe7eedbe2488a1e
PriyankaTayade <ptayade@scu.edu>
2014-08-02 05:01:40 -0700

    client ip

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,17 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import java.net._
+
 
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+ 
+    implicit request =>
+    val address =  request.remoteAddress
+    Logger.info("address " + address)
+  Ok("Got request [" + address + "]")
   }
 
 }
